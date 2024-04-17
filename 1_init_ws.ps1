f1fe62ed33b3abd0c1680de95df6901b8f05471c
guest1111 <guest1111dsa@gmail.com>
2024-04-17 20:44:35 +0900

    change files fam

--- 1_init_ws.ps1
+++ 1_init_ws.ps1
@@ -103,8 +103,3 @@ else {
     Write-Output "WDAC configuration file does not exist."
 }
 
-
--Path C:\Users\Administrator\.ssh\authorized_keys -Value "`r`nssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJGlz+ylWm+INl2uRj61fovI8ihshgnqhlqkvzGI37ns BEG13_BLUE@LS24"
-
-Stop-Service
-
